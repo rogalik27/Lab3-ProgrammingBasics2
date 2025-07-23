48db0994f34fa564f39d790b83f3f71754fa5a00
Micha Ivanchenko <mykhailo.ivanchenko@netways.de>
2025-07-23 10:55:08 +0200

    Add operator- function

--- 1.1.cpp
+++ 1.1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 class Point {
     private:
@@ -12,7 +13,6 @@ class Point {
         Point(double x, double y, double z){
             setCoord(x, y, z);
         }
-
 
         void setCoord(double x,double y,double z){
             this->x = x;
@@ -31,14 +31,32 @@ class Point {
         double getZ(){
             return this->z;
         }
+
+        friend double operator-(const Point& a, const Point& b);
 };
 
-int main(){
-    Point point1;
+double operator-(const Point& a, const Point& b) {
+    double dx = a.x - b.x;
+    double dy = a.y - b.y;
+    double dz = a.z - b.z;
+    return sqrt(dx*dx + dy*dy + dz*dz);
+}
 
+int main() {
+    Point point1;
     Point point2(2,5,2);
 
     cout << point1.getX() << " " << point1.getY() << " " << point1.getZ() << endl;
     cout << point2.getX() << " " << point2.getY() << " " << point2.getZ() << endl;
 
+    double distance1 = point1 - point2;
+    cout << "Distance (неявний operator-): " << distance1 << endl;
+
+    point1.setCoord(1, 1, 1);
+
+    double distance2 = operator-(point1, point2);
+    cout << "Distance (explicit(явний) operator-): " << distance2 << endl;
+
+    return 0;
 }
+
