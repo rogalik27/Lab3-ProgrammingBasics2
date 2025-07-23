5f49b0ee4d438a9015647929d14b9414703dbe9f
Micha Ivanchenko <mykhailo.ivanchenko@netways.de>
2025-07-23 11:15:05 +0200

    Add group class

--- 1.3.cpp
+++ 1.3.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const int MAX_STUDENTS = 50;
 const int SUBJECT_COUNT = 5;
 class Group;
 class Student {
@@ -42,5 +47,60 @@ public:
     }
 };
 
+class Group {
+private:
+    int index;
+    Student* students[MAX_STUDENTS];
+    int count;
+
+public:
+    Group() : index(0), count(0) {}
+
+    Group(int index) : index(index), count(0) {}
+
+    void setIndex(int newIndex) { index = newIndex; }
+    int getIndex() const { return index; }
+    int getCount() const { return count; }
+
+    Student* operator[](int i) const {
+        if (i >= 0 && i < count)
+            return students[i];
+        return nullptr;
+    }
+
+    void addStudent(Student* s) {
+        if (count < MAX_STUDENTS) {
+            students[count++] = s;
+            s->setGroup(this);
+        }
+    }
+
+    void sortByName() {
+        for (int i = 0; i < count - 1; ++i) {
+            for (int j = i + 1; j < count; ++j) {
+                if (*students[i] > *students[j]) {
+                    Student* tmp = students[i];
+                    students[i] = students[j];
+                    students[j] = tmp;
+                }
+            }
+        }
+    }
+
+    Student** findEvenIdStudents(int& foundCount) {
+        Student** found = new Student*[count];
+        foundCount = 0;
+        for (int i = 0; i < count; ++i) {
+            if (students[i]->getId() % 2 == 0)
+                found[foundCount++] = students[i];
+        }
+        return found;
+    }
+
+    void printGroup() const {
+        cout << "Group #" << index << ":\n";
+        for (int i = 0; i < count; ++i)
+            cout << "  " << *students[i] << endl;
+    }
 };
 
